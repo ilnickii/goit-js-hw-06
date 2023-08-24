75e5ce1d829bb6534a7e4b5f28063103ddb7785b
ilnickii <ilnickii2008@gmail.com>
2023-08-24 13:08:33 +0300

    add-task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector('#ingredients');
+
+const markup = ingredients.map((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  li.classList.add('item');
+  console.log(li);
+  return li;
+})
+
+ul.append(...markup);
