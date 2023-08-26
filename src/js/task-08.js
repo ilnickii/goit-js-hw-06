87af311fb56ac2892842cfe812defa555c18675a
ilnickii <ilnickii2008@gmail.com>
2023-08-26 18:53:00 +0300

    add-task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener('submit', loginSubmit);
+
+function loginSubmit (event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === "" || password === "") {
+        alert('All fields must be filled in');
+      } else {
+        const formData = {
+            email,
+            password
+        };
+        console.log(formData);
+        form.reset();
+      }
+    
+      
+}
 
